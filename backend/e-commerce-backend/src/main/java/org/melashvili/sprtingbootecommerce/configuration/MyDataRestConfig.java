90b82f4b764e18b1db11a0715c4cfe8431128d68
Nika Melashvili <melashvilinikusha63@gmail.com>
2024-07-27 22:52:24 +0400

    updated backend for countries and states

--- backend/e-commerce-backend/src/main/java/org/melashvili/sprtingbootecommerce/configuration/MyDataRestConfig.java
+++ backend/e-commerce-backend/src/main/java/org/melashvili/sprtingbootecommerce/configuration/MyDataRestConfig.java
@@ -2,8 +2,10 @@ package org.melashvili.sprtingbootecommerce.configuration;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
+import org.melashvili.sprtingbootecommerce.entity.Country;
 import org.melashvili.sprtingbootecommerce.entity.Product;
 import org.melashvili.sprtingbootecommerce.entity.ProductCategory;
+import org.melashvili.sprtingbootecommerce.entity.State;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -31,18 +33,20 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
         // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        disableHttpMethods(Product.class, config, theUnsupportedActions);
+        disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+        disableHttpMethods(Country.class, config, theUnsupportedActions);
+        disableHttpMethods(State.class, config, theUnsupportedActions);
 
         exposeIds(config);
 
+    }
+
+    private void disableHttpMethods(Class theClass ,RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions){
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
---
+++ backend/e-commerce-backend/src/main/java/org/melashvili/sprtingbootecommerce/entity/Country.java
@@ -0,0 +1,31 @@
+package org.melashvili.sprtingbootecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
+
+}
