1508cee7cd89647ff08c7e42e7cf5a4f6956b58e
Nika Melashvili <melashvilinikusha63@gmail.com>
2024-08-01 01:07:13 +0400

    started saving orders

---
+++ backend/e-commerce-backend/src/main/java/org/melashvili/sprtingbootecommerce/entity/Customer.java
@@ -0,0 +1,30 @@
+package org.melashvili.sprtingbootecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
